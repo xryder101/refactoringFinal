b3417deb040a4939f512e18aa603c7bac87c8356
Samson Eben <seben1@gulls.salisbury.edu>
2018-12-05 12:29:24 -0500

    checked if rental works which it does

--- src/TestJUnit.java
+++ src/TestJUnit.java
@@ -2,13 +2,22 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TestJUnit {
-   @Test
+  Movie movie = new Movie ("Avengers", 1);
+  Rental rent = new Rental (movie, 1);
+	@Test
 	
-   public void testAdd() {
-      String str = "Junit is working fine";
-      assertEquals("Junit is working fine",str);
+   public void movieTest() {
+      String str = "Avengers";
+      assertEquals(movie.getTitle(),str);
    }
-   
-   
+    
+	@Test
+	
+	public void rentalTest() {
+		 
+		 assertEquals(rent.getMovie(), movie);
+		 assertEquals(rent.getDaysRented(), 1);
+	}
+  
    
 }
