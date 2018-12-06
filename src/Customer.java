d32ca51f432065b2dde352af0f3eae03bbe3b2f2
Samson Eben <seben1@gulls.salisbury.edu>
2018-12-06 15:24:21 -0500

    Replace Temp with Query

--- src/Customer.java
+++ src/Customer.java
@@ -26,18 +26,17 @@ public class Customer {
 
         String result = "Rental record for " + getName() + "\n";
         for (Rental rental : rentals) {
-        	double amount = rental.getCharge();
+        	
 
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
             if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
                 frequentRenterPoints++;
-
             // show figures for this rental
-            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(amount) + "\n";
+            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getCharge()) + "\n";
 
-            totalAmount += amount;
+            totalAmount += rental.getCharge();
         }
 
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
