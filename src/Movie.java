ea3eda9941b5e1799121c3862575f8100c6f03cb
Samson Eben <seben1@gulls.salisbury.edu>
2018-12-07 14:23:57 -0500

    Replace Conditional with Polymorphism/ Complete the final refactoring

--- src/Movie.java
+++ src/Movie.java
@@ -44,10 +44,8 @@ public class Movie {
     	return price.getCharge(daysRented);
     }
     public int getFrequentRenterPoints(int daysRented) {
-        if (getPriceCode() == Movie.NEW_RELEASE && daysRented > 1)
+        return price.getFrequentRenterPoints(daysRented);
 
-    	return 2;
-        return 1;
     }
 
 }
--- src/NewReleasePrice.java
+++ src/NewReleasePrice.java
@@ -3,4 +3,14 @@ public class NewReleasePrice extends Price {
     public int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+     @Override
+     public double getCharge(int daysRented) {
+         return daysRented * 3;
+     }
+     @Override
+     int getFrequentRenterPoints(int daysRented) {
+         if (daysRented > 1)
+             return 2;
+         return 1;
+     }
 }
