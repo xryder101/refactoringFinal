c1cfcc79e7a50ebb002d211e5528b1b097c97546
Samson Eben <seben1@gulls.salisbury.edu>
2018-12-07 13:53:30 -0500

    inheritance (continued)

--- src/Movie.java
+++ src/Movie.java
@@ -7,7 +7,8 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
 
     private String title;
-    private int priceCode;
+    private Price price;
+
 
     public Movie(String title, int priceCode) {
         this.title = title;
@@ -17,13 +18,31 @@ public class Movie {
     public String getTitle() {
         return title;
     }
-    public void setPriceCode(int priceCode) {
-        this.priceCode = priceCode;
+    private void setPriceCode(int priceCode) {
+    	switch (priceCode) {
+        case CHILDREN:
+            price = new ChildrensPrice();
+            break;
+        case NEW_RELEASE:
+            price = new NewReleasePrice();
+            break;
+        case REGULAR:
+            price = new RegularPrice();
+            break;
+        default:
+            throw new IllegalArgumentException("invalid price code");
+    }
     }
    
+    private int getPriceCode() {
+        return price.getPriceCode();
+
+    }
+    
     public double getCharge(int daysRented) {
         double amount = 0;
-        switch (priceCode) {
+        switch (getPriceCode()) {
+
             case REGULAR:
                 amount += 2;
                 if (daysRented > 2)
@@ -41,8 +60,9 @@ public class Movie {
         return amount;
     }
     public int getFrequentRenterPoints(int daysRented) {
-        if (priceCode == Movie.NEW_RELEASE && daysRented > 1)
-            return 2;
+        if (getPriceCode() == Movie.NEW_RELEASE && daysRented > 1)
+
+    	return 2;
         return 1;
     }
 
